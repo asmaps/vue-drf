4ef7c072e6ddb6f9e2f4bebaac788eccabc6cf24
Arne Schauf <arne.schauf@globalways.net>
2016-06-28 17:04:11 +0200

    try to rename main for loading issue

--- main.js
+++
@@ -1,9 +0,0 @@
-import DrfInlineEditable from './DrfInlineEditable'
-import DrfEditableModel from './DrfEditableModel'
-
-export default {
-  install (Vue, options) {
-    Vue.component('drf-inline-editable', DrfInlineEditable)
-    Vue.component('drf-editable-model', DrfEditableModel)
-  }
-}
---
+++ main.vue
@@ -0,0 +1,11 @@
+<script>
+  import DrfInlineEditable from './DrfInlineEditable'
+  import DrfEditableModel from './DrfEditableModel'
+
+  export default {
+    install (Vue, options) {
+      Vue.component('drf-inline-editable', DrfInlineEditable)
+      Vue.component('drf-editable-model', DrfEditableModel)
+    }
+  }
+</script>
